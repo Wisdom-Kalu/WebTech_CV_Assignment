04dc5548288d90e0d549424c8fc0094d52cb5092
wisdom <wisdom.kalu@ashesi.edu.gh>
2020-09-19 12:37:27 +0000

    cocluded my coding assignment. fixed all bugs

---
+++ cv.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Coded with love
+ * File: cv.js
+ * File Created: Wednesday, 16th 2020 10:01 AM
+ * @author Wisdom Tochi Kalu
+ * @version 1.0
+ * Modified By: Wisdom Tochi Kalu
+ * Last Modified: Saturday, 29 September 2020 11:00 AM
+ * Copyright @2020 Wisdom Tochi Kalu
+ * 
+ */
+
+
+
+    //displaying the value from local storage to the cv.html page by their respective Ids
+    document.getElementById("formdata1").innerHTML=window.localStorage.getItem("formval1");
+    document.getElementById("formdata2").innerHTML=window.localStorage.getItem("formval2");
+    document.getElementById("formdata3").innerHTML=window.localStorage.getItem("formval3");
+    document.getElementById("formda1").innerHTML=window.localStorage.getItem("formval4");
+    document.getElementById("formda2").innerHTML=window.localStorage.getItem("formval5");
+    document.getElementById("formda3").innerHTML=window.localStorage.getItem("formval6");
+    document.getElementById("formda4").innerHTML=window.localStorage.getItem("formval7");
+    
